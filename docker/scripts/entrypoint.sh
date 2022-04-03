a2b597e2862cea45d5963229c00631210e949795
Daniel Habowski <daniel.habowski@metapack.com>
2022-04-03 23:00:35 +0200

    Docker introduced

---
+++ docker/scripts/entrypoint.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+if [ "$1" = 'bedrock_server' ]; then
+  MaxGracefulTime=${BEDROCK_IN_DOCKER_TERM_MIN:-1}
+  trap '{ echo "recive SIGTERM. Going to stop bedrock immediately."; /scripts/terminate.sh 0 || true; exit 0; }' SIGTERM
+  trap '{ echo "recive SIGQUIT. Going to stop bedrock in $MaxGracefulTime minute(s)."; /scripts/terminate.sh $MaxGracefulTime || true; exit 0; }' SIGQUIT
+
+  echo 'Starting bedrock-in-docker deamon...'
+  rm -f /bedrock/bedrock_screen.log
+  tail -f --retry --sleep-interval=1 --max-unchanged-stats=300 /bedrock/bedrock_screen.log 2> /dev/null &
+  screen -wipe
+
+  while true
+  do
+    MaxGracefulTime=${BEDROCK_IN_DOCKER_TERM_MIN:-1}
+
+    (
+      /scripts/terminate.sh $MaxGracefulTime || true
+      /scripts/update.sh || true
+
+      if ! screen -list | grep -q "bedrock"; then
+        screen -d -m -S bedrock -L -Logfile /bedrock/bedrock_screen.log bash -c "/scripts/start.sh"
+      else
+        echo 'FATAL: Unexpected screen bedrock already there'
+      fi
+    ) || echo 'Some error occured'
+
+    RestartTime=${BEDROCK_IN_DOCKER_RESTART_TIME_UTC}
+    current_epoch=$(date +%s)
+    target_epoch=$(date -d $RestartTime +%s)
+
+    sleep_seconds=$(( $target_epoch - $current_epoch ))
+    if (( $sleep_seconds <= 300 ))
+    then
+      sleep_seconds=$(( $sleep_seconds + 86400))
+    fi
+    echo "Bedrock will run for next $sleep_seconds seconds"
+    if [ $BEDROCK_IN_DOCKER_FORCE_1_MIN_RESTART == "1" ]
+    then
+      echo "Forcing 1min restarts."
+      sleep_seconds=60
+    fi
+    sleep $sleep_seconds &
+    wait $!
+  done
+fi
+exec "$@"
---
+++ docker/scripts/update.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Test internet connectivity first
+wget --quiet -U "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)" --timeout=30 http://www.minecraft.net/ -O /dev/null
+if [ "$?" != 0 ]; then
+    echo "Unable to connect to update website (internet connection may be down).  Skipping update ..."
+else
+    if [ "$BEDROCK_DOWNLOAD_URL" ]; then
+      # Use specified version
+      DownloadURL="$BEDROCK_DOWNLOAD_URL"
+    else
+      # Download server index.html to check latest version
+      wget --no-verbose -U "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)" --timeout=30 -O /downloads/version.html https://minecraft.net/en-us/download/server/bedrock/
+      DownloadURL=$(grep -o 'https://minecraft.azureedge.net/bin-linux/[^"]*' /downloads/version.html)
+    fi
+    DownloadFile=$(echo "$DownloadURL" | sed 's#.*/##')
+
+    # Download latest version of Minecraft Bedrock dedicated server if a new one is available
+    if [ -f "/downloads/$DownloadFile" ]
+    then
+        echo "Minecraft Bedrock server is up to date..."
+    else
+        echo "New version $DownloadFile is available.  Updating Minecraft Bedrock server ..."
+        rm -f /downloads/*
+        wget --no-verbose -O "/downloads/$DownloadFile" "$DownloadURL"
+        if [ -f "/bedrock/server.properties" ]
+        then
+          unzip -q -o "/downloads/$DownloadFile" -x "*server.properties*" "*permissions.json*" "*whitelist.json*" -d /bedrock/
+        else
+          unzip -q -o "/downloads/$DownloadFile" -d /bedrock/
+        fi
+        chmod +x /bedrock/bedrock_server
+    fi
+fi
