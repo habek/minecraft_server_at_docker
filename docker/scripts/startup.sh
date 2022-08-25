db819da401caf5effe08ac8bbde909c3a518524c
Daniel Habowski <daniel.habowski@auctane.com>
2022-08-25 22:40:23 +0200

    updating server on docker startup

--- docker/scripts/startup.sh
+++ docker/scripts/startup.sh
@@ -3,5 +3,5 @@ if [ -d "/bedrock/worlds/backup_restore" ]; then
     rm -r "/bedrock/worlds/Bedrock level"
     mv -v "/bedrock/worlds/backup_restore" "/bedrock/worlds/Bedrock level"
 fi
-# /scripts/update.sh
+/scripts/update.sh
 /bedrock/bedrock_server
