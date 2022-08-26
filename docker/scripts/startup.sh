7dca2ac72ee95cad916cddf510d6d961e14b5473
Daniel Habowski <daniel.habowski@auctane.com>
2022-08-26 23:39:29 +0200

    Messages about backup restore added

--- docker/scripts/startup.sh
+++ docker/scripts/startup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 if [ -d "/bedrock/worlds/backup_restore" ]; then
+    echo Restoring world from backup...
     rm -r "/bedrock/worlds/Bedrock level"
     mv -v "/bedrock/worlds/backup_restore" "/bedrock/worlds/Bedrock level"
+    echo Backup restored
 fi
 /scripts/update.sh
 /bedrock/bedrock_server
